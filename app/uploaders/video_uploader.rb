3500486975857b9430de817a74a124cfd5e7f964
jocietimes <jocelyn.mellein@outlook.com>
2020-10-18 21:48:16 +0000

    did not use resolution changer for images--will need to return to that later- got s3 working by makingit public created video uploader, uploaded test video. video doesn't auto play but it downloads

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [800, 350]
+  # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
