8ebb54ae92b4866529d11c9668cd0709fb270d72
jocietimes <jocelyn.mellein@outlook.com>
2020-10-24 20:04:21 +0000

    used jquery on lesson modal. removed all traces of the new method for lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,11 +1,8 @@
 class Instructor::LessonsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorized_for_current_section, only: [:new, :create]
+    before_action :require_authorized_for_current_section, only: [:create]
     before_action :require_authorized_for_current_lesson, only: [:update]
 
-    def new
-        @lesson = Lesson.new
-    end
 
     def create
         @lesson = current_section.lessons.create(lesson_params)
@@ -14,6 +11,7 @@ class Instructor::LessonsController < ApplicationController
 
     def update
         current_lesson.update_attributes(lesson_params)
+        render plain: 'updated!'
     end
 
     private
